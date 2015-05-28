b2c4ad15d589ce0f1e06da88f663621e739de29e
Fred <fredpaw@gmail.com>
2015-05-28 13:50:56 +1000

    all done
    include all the resource files.

--- themes/all.js
+++ themes/all.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function($) {
+	
+	//Menu animation
 	$("#header-button").click(function() {
 		$("#header-menu").toggle(200);
 	});
@@ -7,5 +9,32 @@ jQuery(document).ready(function($) {
 	});
 	$("#page-content").mousedown(function() {
 		$("#header-menu").hide(200);
+	});
+	
+	//Account center score animation
+	$(".account-bg .unsign").click(function() {
+		$(this).addClass("no-show");
+		$(".account-bg .sign-score").removeClass("no-show");
+	});
+	
+	//Input textarea animation
+	$(".input-comment").click(function() {
+		if($(this).val() == "请输入文字") {
+			$(this).val("");
+			$(this).removeClass("comment-light");
+		}
+	});
+	
+	$(".input-comment").blur(function() {
+		if($(this).val() == "") {
+			$(this).addClass("comment-light");
+			$(this).val("请输入文字");
+		}
+	});
+	
+	//file upload button function
+	$("#file-upload").on('change',function() {
+		var path = $(this).val();
+		$("#file-name").text(path);
 	});
 });
